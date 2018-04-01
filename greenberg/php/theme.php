33739e55f1d1cb38bb67744573417dbf08579132
Erik Ridderby <erik@ridderby.se>
2018-04-01 07:50:06 +0200

    Further changes to make the theme work.

--- greenberg/php/theme.php
+++ greenberg/php/theme.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- *   * Name: Suckerberg
- *   * Description: A Facebook-like theme based on redbasic
+ *   * Name: Greenberg
+ *   * Description: A Facebook-like theme based on Suckenberg in turn based on redbasic
  *   * Version: 2.1
  *   * MinVersion: 2.3.1
  *   * MaxVersion: 3.5
- *   * Author: Sean Tilley
+ *   * Author: Sean Tilley modified by Erik Ridderby
  *   * Compat: Hubzilla [*]
  *
  */
 
-function suckerberg_init(&$App) {
+function greenberg_init(&$App) {
 
     App::$theme_info['extends'] = 'redbasic';
 
